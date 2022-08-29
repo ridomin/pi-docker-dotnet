bc85f1d94f3c0de2a0d8eb95cbebbb2e456008db
Rido <ridomin@live.com>
2022-08-29 14:02:11 +0000

    pi image

--- b.sh
+++ b.sh
@@ -1,1 +1,2 @@
-docker build --tag dotnet-app:arm -f dotnet-app/Dockerfile.pi dotnet-app
+docker build --tag ghcr.io/ridomin/dotnet-app:arm32 -f dotnet-app/Dockerfile.pi dotnet-app
+docker build --tag ghcr.io/ridomin/dotnet-app:amd64 -f dotnet-app/Dockerfile dotnet-app
